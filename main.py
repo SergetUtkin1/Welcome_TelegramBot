be3da4409d7aa5d60e1c6b652029704397476d48
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-08-28 23:31:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+TOKEN = '5475930755:AAFbqdkwZiKBdlolzZg7VA6q37oJHMGCCbA'
+TEXT = 'hi'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.chat_join_request_handler()
+async def echo(message: types.Message):
+    await bot.approve_chat_join_request(
+            message.chat.id,
+            message.from_user.id
+        )
+    await bot.send_message(
+            message.from_user.id,
+            TEXT
+        )
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
